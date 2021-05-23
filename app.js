fc855a795b8e646fe95636e8f9b4417d9fd011a7
raquellvazquez <raquelskrats@gmail.com>
2021-05-23 15:06:53 -0500

    feat:  web server

--- app.js
+++ app.js
@@ -4,10 +4,6 @@ const port = 8080;
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html')
-})
-
 app.listen(port, () => {
     console.log(`Turi app listening at http://localhost:${port}`)
   })
