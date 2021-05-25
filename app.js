6e11e31c2515d4f036775df05d3f6fb1b2a4a13f
raquellvazquez <raquelskrats@gmail.com>
2021-05-24 20:15:49 -0500

    solve path

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ const express = require('express')
 const app = express()
 const port = 8080;
 
+const path = require('path');
+
 app.use(express.static('public'));
+
+app.get('*', function(req, res) {
+  res.sendFile('index.html', {root: path.join(__dirname, '/public/')});
+});
 
 app.listen(port, () => {
     console.log(`Turi app listening at http://localhost:${port}`)
